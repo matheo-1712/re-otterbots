b9cd0f81ea5ee7ca7f6c28ac6d988389d03b8a71
matheo-1712 <perod.matheo@gmail.com>
2026-09-08 01:43:35 +0200

    Add project dependencies and configuration files to initialize repository setup
    Signed-off-by: matheo-1712 <perod.matheo@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export class Otterbots {
+  // Token du bot
+  private readonly BOT_TOKEN: string;
+
+  constructor(token: string) {
+    this.BOT_TOKEN = token;
+  }
+
+  // Démarre le bot
+  start(): void {
+
+  }
+
+
+}
